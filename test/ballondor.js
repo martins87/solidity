4fe6a4732e7e1c9e03b93467fa2315b96a8b9f60
Daniel Martins <martinsda@gmail.com>
2018-11-29 10:59:49 -0200

    Update ballondor.js

--- test/ballondor.js
+++ test/ballondor.js
@@ -1,65 +1,94 @@
-var BallondOr = artifacts.require("./BallondOr.sol");
+var BallondOr = artifacts.require("BallondOr.sol");
 
 contract("BallondOr", function() {
 
-  var ballondOrInstance;
+  var ballondorInstance;
   var candidateId;
   var account;
 
-  it("Inicializa seis candidatos", function() {
+  // testar inicialização de 6 candidatos
+  it("Inicializa 6 candidatos", function() {
     /* testar:
         - número correto de candidatos inicializados
     */
     return BallondOr.deployed().then(function(instance) {
-      ballondOrInstance = instance;
-      return ballondOrInstance.totalCandidates();
+      return instance.totalCandidates();
     }).then(function(totalCandidates) {
-      assert.equal(totalCandidates, 6, "Inicializou incorretamente o número de candidatos");
+      assert.equal(totalCandidates, 6);
     });
   });
 
-  it("Instancializa os candidatos com os valores corretos", function() {
+  // testar inicialização correta dos 6 candidatos
+  it("Inicializa os 6 candidatos com os valores corretos", function() {
     /* testar:
-        - valores inicializados corretamente
+        - inicialização correta dos 3 valores de cada candidato
     */
     return BallondOr.deployed().then(function(instance) {
-      ballondOrInstance = instance;
-      return ballondOrInstance.candidates(1);
+      ballondorInstance = instance;
+      return ballondorInstance.candidates(1);
     }).then(function(candidate) {
       assert.equal(candidate[0], 1);
       assert.equal(candidate[1], "Lionel Messi");
       assert.equal(candidate[2], 0);
-      return ballondOrInstance.candidates(2);
+      return ballondorInstance.candidates(2);
+    }).then(function(candidate) {
+      assert.equal(candidate[0], 2);
+      assert.equal(candidate[1], "Cristiano Ronaldo");
+      assert.equal(candidate[2], 0);
+      return ballondorInstance.candidates(3);
+    }).then(function(candidate) {
+      assert.equal(candidate[0], 3);
+      assert.equal(candidate[1], "Neymar Jr");
+      assert.equal(candidate[2], 0);
+      return ballondorInstance.candidates(4);
+    }).then(function(candidate) {
+      assert.equal(candidate[0], 4);
+      assert.equal(candidate[1], "Kylian Mbappe");
+      assert.equal(candidate[2], 0);
+      return ballondorInstance.candidates(5);
+    }).then(function(candidate) {
+      assert.equal(candidate[0], 5);
+      assert.equal(candidate[1], "Luka Modric");
+      assert.equal(candidate[2], 0);
+      return ballondorInstance.candidates(6);
+    }).then(function(candidate) {
+      assert.equal(candidate[0], 6);
+      assert.equal(candidate[1], "Mohammed Salah");
+      assert.equal(candidate[2], 0);
     });
-  });
+  })
 
-  it("Verifica voto correto em um candidato", function() {
-    /* testar 3 coisas:
-        - evento disparado
-        - número de votos do candidato é o esperado
+  // testar o registro de um voto
+  it("Registra corretamente o voto", function() {
+    /* testar:
+        - candidato recebeu 1 voto a mais do que tinha anteriormente
         - hasAddressVoted foi marcado como true
+        - evento Voted foi emitido
     */
     return BallondOr.deployed().then(function(instance) {
-      ballondOrInstance = instance;
-      account = web3.eth.accounts[0];
+      ballondorInstance = instance;
       candidateId = 1;
-      return ballondOrInstance.vote(candidateId, { from: account });
+      account = web3.eth.accounts[0];
+      return ballondorInstance.vote(candidateId, { from: account });
     }).then(function(receipt) {
+      /* verificar:
+          - que há um evento registrado no log
+          - que o evento registado foi o correto
+          - que o argumento passado foi o correto
+      */
       assert.equal(receipt.logs.length, 1);
-      assert.equal(receipt.logs[0].event, "Voted");
-      //console.log(receipt.logs[0].args._candidateId.toNumber());
-      assert.equal(receipt.logs[0].args._candidateId, 1);
-      return ballondOrInstance.hasAddressVoted(account);
+      assert.equal(receipt.logs[0].event, 'Voted');
+      assert.equal(receipt.logs[0].args._candidateId, '1');
+      return ballondorInstance.hasAddressVoted(account);
     }).then(function(voted) {
       assert.equal(voted, true);
-      return ballondOrInstance.candidates(candidateId);
+      return ballondorInstance.candidates(candidateId);
     }).then(function(candidate) {
-      assert.equal(candidate[2], 1);
+      assert.equal(candidate[2].toNumber(), 1);
     });
   });
-
-  // testar revert() em voto em candidato inválido
-
-  // testar exceção para voto duplo
+  
+  // testar lançamento de exceção para voto em candidato inválido
+  // testar lançamento de exceção para voto duplo do mesmo endereço
 
 });
